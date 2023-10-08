444d1e06e4c8b61f0886de499518d2cc7dc910ae
Alexander Riabinin <ryabinintm@yandex.ru>
2023-10-08 15:00:55 +0300

    clean hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,12 +3,6 @@
 
 #include <iostream>
 
-/*
-void todo_move_me_out() {
-	std::cout << "This function should be located in the separated source file" << std::endl;
-}
-*/
-
 int main() {
 	std::cout << "Hello, World!" << std::endl;
 
